5f2bd2ada1a625edbddf6727d62001695af7d58f
Lyapina Ekaterina <72204301+18kate@users.noreply.github.com>
2024-06-15 13:36:08 +0300

    Update ProfileScreen.js

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 import Card from '../../src/components/TeamderCard/index'; // Импортируем компонент Card
-import users from '../../assets/data/users';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { supabase } from '../lib/supabase'; // Импортируем supabase
 
 const ProfileScreen = () => {
-  const user = users[12]; 
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        // Загрузка текущего пользователя из AsyncStorage
+        const user = await AsyncStorage.getItem('currentUser');
+        if (user) {
+          setCurrentUser(JSON.parse(user));
+        }
+      } catch (error) {
+        console.error('Ошибка загрузки текущего пользователя:', error);
+      }
+    };
+
+    fetchCurrentUser();
+  }, []);
+
+  if (!currentUser) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Card user={user} />
+      <Card user={currentUser} />
     </View>
   );
 };
@@ -20,7 +45,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginLeft: 20,
-
   },
 });
 
